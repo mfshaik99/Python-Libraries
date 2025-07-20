28c3b96432aabb2097d64c5f3402a5653746a7fd
MUHAMMAD FARHAN SHAIK <24B81A66E3@cvr.ac.in>
2025-07-20 22:11:08 +0530

    Linespace.py

---
+++ Numpy/Linespace.py
@@ -0,0 +1,7 @@
+import numpy as np  # Import NumPy
+
+arr1 = np.linspace(0, 1, 5)  # 5 values from 0 to 1
+print("Linspace 0 to 1:", arr1)
+
+arr2 = np.arange(1, 10, 2)   # Values from 1 to 9 with step 2
+print("Arange 1 to 9:", arr2)
