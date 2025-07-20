441aadb18c02df4a892e91bd0ab2a63bed79cb5c
MUHAMMAD FARHAN SHAIK <24B81A66E3@cvr.ac.in>
2025-07-20 22:12:24 +0530

    AggregateFunctions.py

---
+++ Numpy/AggregateFunctions.py
@@ -0,0 +1,11 @@
+import numpy as np  # Import NumPy
+
+arr = np.array([4, 7, 1, 8, 3])
+
+print("Array:", arr)
+
+print("Min:", np.min(arr))      # Minimum value
+print("Max:", np.max(arr))      # Maximum value
+print("Mean:", np.mean(arr))    # Average
+print("Std Dev:", np.std(arr))  # Standard deviation
+print("Sum:", np.sum(arr))      # Total sum
